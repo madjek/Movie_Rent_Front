dc95c6a5a9730c63c54afa89010ef3fc5e52bacc
madjek <90720831+madjek@users.noreply.github.com>
2021-11-21 19:24:44 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.scss';
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './Containers/Home/Home';
+import Login from './Containers/Login/Login';
+import Movie from './Containers/Movie/Movie';
+import Header from './Components/Header/Header';
+import Profile from './Containers/Profile/Profile';
+import Register from './Containers/Register/Register';
+
+function App() {
+  return (
+    <div className="App">
+
+      <BrowserRouter>
+
+      <Header/>
+
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/movie" element={<Movie/>}/>
+
+        </Routes>
+      
+      </BrowserRouter>
+      
+    </div>
+  );
+};
+
+export default App;
