1e06d7811b333d7011d3c23efad3254881206d1a
madjek <90720831+madjek@users.noreply.github.com>
2021-11-21 19:33:46 +0100

    header component added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { LOGOUT } from '../../redux/types';
+
+import logo from '../../assets/img/logo.svg';
+
+
+// import './Header.scss';
+import Button from '../Button/Button';
+
+const Header = (props) => {
+
+    const logOut = () => {        
+        props.dispatch({type:LOGOUT});
+        window.location.href = '/';
+    }
+
+    if (props.credentials?.token === '') {
+        return(
+            <div className="designHeader b_row">
+                <div className="logo b_row">
+                    <img className="logoImg" alt="logo" src={logo}/>
+                </div>
+                <div className="nav b_row">
+                    <Button destination="Home" url=""/>
+                    <Button destination="Login" url="/login"/>
+                    <Button destination="Register" url="/register"/>
+                </div>
+            </div>
+        )
+    } else {
+        return(
+            <div className="designHeader b_row">
+            <div className="logo b_row">
+                <img className="logoImg" alt="logo" src={logo}/>
+            </div>
+            <div className="nav b_row">
+                <Button destination="Home" url=""/>
+                <Button destination="Profile" url="/profile"/>
+                <div className="logoutButton btn b_row" onClick={() => logOut()}>Logout</div>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default connect((state)=>({
+    credentials: state.credentials
+}))(Header);
